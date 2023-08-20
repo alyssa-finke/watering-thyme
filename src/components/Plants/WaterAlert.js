ec9a47d08e802f1a9041fa4167e396581ca845cb
“Alyssa <alyssa.storey14@gmail.com>
2023-08-20 16:40:10 -0400

    add button to dismiss entire alert section

--- src/components/Plants/WaterAlert.js
+++ src/components/Plants/WaterAlert.js
@@ -5,21 +5,27 @@ import WaterList from './WaterList';
 
 
 const WaterAlert = props => {
+    const [showAlert, toggleShowAlert] = useState(true);
+
     return (
-        <Card className="alert-card">
-            <div>
-                <div>
-                    <h2 className="alert-message">These plants are thirsty!</h2>
-                    {props.plants.map((plantInfo) => (
-                        <WaterList
-                            key={plantInfo.id}
-                            name={plantInfo.name}
-                            firstWater={plantInfo.firstWater}
-                        />
-                    ))}
-                </div>
-            </div>
-        </Card>
+        <div>
+            {showAlert ?
+                <Card className="alert-card">
+                    <button onClick={() => toggleShowAlert(!showAlert)}>X</button>
+                    <div className="alert-message">
+                        <div>
+                            <h2>These plants are thirsty!</h2>
+                            {props.plants.map((plantInfo) => (
+                                <WaterList
+                                    key={plantInfo.id}
+                                    name={plantInfo.name}
+                                    firstWater={plantInfo.firstWater}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </Card> : null}
+        </div>
     );
 }
 
