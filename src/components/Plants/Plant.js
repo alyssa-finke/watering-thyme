e1704018a92b3cd5818a1cdcaaf2058de84047cf
“Alyssa <alyssa.storey14@gmail.com>
2024-04-21 18:29:24 -0400

    remove water frequency on plant

--- src/components/Plants/Plant.js
+++ src/components/Plants/Plant.js
@@ -13,7 +13,7 @@ const Plant = (props) => {
                 <FirstWater firstWater={props.firstWater} />
                 <div className="plant-info">
                     <h2>{props.name}</h2>
-                    <div className="water-frequency">{props.frequency}</div>
+                    {/*<div className="water-frequency">{props.frequency}</div> */}
                 </div>
                 <button id={props.id} className="delete-button" onClick={() => handleDeleteItem(props.id)}>Delete</button>
             </Card>
